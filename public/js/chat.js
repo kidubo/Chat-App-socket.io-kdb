315a2886b74ff189e06367d35866b9be6e7eb1bc
kidubo <innocentleonard007@gmail.com>
2020-10-07 04:23:33 +0300

    event acknowledgement socket.io

--- public/js/chat.js
+++ public/js/chat.js
@@ -26,7 +26,12 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 
     const message = e.target.elements.message.value
 
-    socket.emit('sendMessage', message)
+    socket.emit('sendMessage', message, (error) => {
+        return console.log(error)
+    })
+
+    console.log('Message was delivered')
+
 })
 
 document.querySelector('#send-location').addEventListener('click', () => {
@@ -40,6 +45,8 @@ document.querySelector('#send-location').addEventListener('click', () => {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
 
+        }, () => {
+            console.log('Location shared!')
         })
     })
 })
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
-const { clear } = require('console')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -12,38 +12,38 @@ const port = process.env.PORT || 2020
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
-
-// let count = 0
-
-// Server (emit) -> Client  (receive)- countUpdated
-// Client (emit) -> Server (receive) - increment
-
-
+    // let count = 0
+    // Server (emit) -> Client  (receive)- countUpdated
+    // Client (emit) -> Server (receive) - increment
 
 io.on('connection', (socket) => {
     // console.log(`New WebSocket connection`)
-
     // socket.emit('countUpdated', count)
-
     // socket.on('increment', () => {
     //     count++
-
     //     // socket.emi emit to a particular client
     //     // but using io.emit emit to everyone in the connection
-
     //     io.emit('countUpdated', count)
-
     // })
 
     socket.emit("message", "welcome!")
     socket.broadcast.emit('message', 'A new user has joined!')
 
-    socket.on('sendMessage', (message) => {
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profenity is not allowed!')
+
+        }
+
         io.emit('message', message)
+        callback('Delivered')
     })
 
-    socket.on('sendLocation', (location) => {
+    socket.on('sendLocation', (location, callback) => {
         io.emit('message', `https://google.com/maps?q=${location.latitude},${location.longitude}`)
+        callback()
     })
 
     socket.on('disconnect', () => {
